a3932bb7426f8f883d083485869cbdb90c4021e4
Lee Maguire <lee.maguire@mongodb.com>
2024-01-25 17:32:29 +0000

    point to vcpkg branch on realm-cpp

--- ports/cpprealm/portfile.cmake
+++ ports/cpprealm/portfile.cmake
@@ -1,6 +1,7 @@
 set(GIT_URL "git@github.com:realm/realm-cpp.git")
-set(GIT_REV "43a68815c11eb7fdd8010eef91157419d2e17b64")
+set(GIT_REV "05b48476dfd0a37d6d6f44ff81711be22699ccf1")
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/realm-cpp)
+find_program(GIT git)
 
 if(NOT EXISTS "${SOURCE_PATH}/.git")
     vcpkg_execute_required_process(
@@ -11,7 +12,12 @@ if(NOT EXISTS "${SOURCE_PATH}/.git")
 
 endif()
 
-find_program(GIT git)
+vcpkg_execute_required_process(
+  COMMAND ${GIT} checkout lm/vcpkg
+  WORKING_DIRECTORY ${SOURCE_PATH}
+  LOGNAME checkout
+)
+
 vcpkg_execute_required_process(
   COMMAND ${GIT} submodule update --init --recursive
   WORKING_DIRECTORY ${SOURCE_PATH}
@@ -32,14 +38,7 @@ vcpkg_cmake_install()
 
 message(${VCPKG_LIBRARY_LINKAGE})
 
-# vcpkg_cmake_config_fixup(PACKAGE_NAME "cpprealm" CONFIG_PATH "share/cpprealm" DO_NOT_DELETE_PARENT_CONFIG_PATH)
-# if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-#     vcpkg_cmake_config_fixup(PACKAGE_NAME "libcpprealm" CONFIG_PATH "lib/cmake/libcpprealm")
-#     file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/libcpprealm-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/cpprealm")
-# else()
-#     message("not static")
-#     # vcpkg_cmake_config_fixup(PACKAGE_NAME "libcpprealm" CONFIG_PATH "lib/cmake/libcpprealm")
-# endif()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "cpprealm" CONFIG_PATH "cmake")
 
 file(REMOVE_RECURSE 
     "${CURRENT_PACKAGES_DIR}/debug/include"
