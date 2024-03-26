2391ff136e26d03ddd552de1d93cc6606d57597f
Lee Maguire <lee.maguire@mongodb.com>
2024-03-26 14:39:51 +0000

    Add cpprealm to vcpkg

--- ports/cpprealm/portfile.cmake
+++ ports/cpprealm/portfile.cmake
@@ -1,46 +1,54 @@
-# set(GIT_URL "git@github.com:realm/realm-cpp.git")
-# set(GIT_REV "05b48476dfd0a37d6d6f44ff81711be22699ccf1")
-# set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/realm-cpp)
-set(SOURCE_PATH "/Users/lee.maguire/desktop/realm-cpp")
-message(SOURCE: ${SOURCE_PATH})
+cmake_path(GET CMAKE_CURRENT_LIST_DIR PARENT_PATH SOURCE_PATH)
 
 find_program(GIT git)
 
-# if(NOT EXISTS "${SOURCE_PATH}/.git")
-#     vcpkg_execute_required_process(
-#         COMMAND ${GIT} clone ${GIT_URL} 
-#         WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
-#         LOGNAME clone
-#     )
+vcpkg_execute_required_process(
+  COMMAND ${GIT} submodule update --init --recursive
+  WORKING_DIRECTORY ${SOURCE_PATH}
+  LOGNAME submodules
+)
 
-# endif()
+set(CPPREALM_CMAKE_OPTIONS -DREALM_CPP_NO_TESTS=ON -DREALM_CORE_SUBMODULE_BUILD=OFF)
 
-# vcpkg_execute_required_process(
-#   COMMAND ${GIT} checkout lm/vcpkg
-#   WORKING_DIRECTORY ${SOURCE_PATH}
-#   LOGNAME checkout
-# )
-
-# vcpkg_execute_required_process(
-#   COMMAND ${GIT} submodule update --init --recursive
-#   WORKING_DIRECTORY ${SOURCE_PATH}
-#   LOGNAME submodules
-# )
+if (ANDROID OR WIN32 OR CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    list(APPEND CPPREALM_CMAKE_OPTIONS -DREALM_USE_SYSTEM_OPENSSL=ON)
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    DISABLE_PARALLEL_CONFIGURE
     OPTIONS
-        -DREALM_CPP_NO_TESTS=ON
+        ${CPPREALM_CMAKE_OPTIONS}
 )
 
 vcpkg_cmake_install()
 
-message(${VCPKG_LIBRARY_LINKAGE})
-
 vcpkg_cmake_config_fixup(PACKAGE_NAME "cpprealm" CONFIG_PATH "cmake")
 vcpkg_cmake_config_fixup(PACKAGE_NAME "Realm" CONFIG_PATH "share/cmake/Realm")
 
-file(REMOVE_RECURSE 
+file(READ ${CURRENT_PACKAGES_DIR}/debug/include/cpprealm/internal/bridge/bridge_types.hpp DEBUG_TYPE_HEADER_CONTENTS)
+set(REGEX_PATTERN "\\{([^()]*)\\}")
+string(REGEX MATCHALL "${REGEX_PATTERN}" MATCHED_CONTENT "${DEBUG_TYPE_HEADER_CONTENTS}")
+set(MATCHED_DEBUG_TYPE_HEADER_CONTENTS "${CMAKE_MATCH_1}")
+
+file(READ ${CURRENT_PACKAGES_DIR}/include/cpprealm/internal/bridge/bridge_types.hpp RELEASE_TYPE_HEADER_CONTENTS)
+set(REGEX_PATTERN "\\{([^()]*)\\}")
+string(REGEX MATCHALL "${REGEX_PATTERN}" MATCHED_CONTENT "${RELEASE_TYPE_HEADER_CONTENTS}")
+set(MATCHED_RELEASE_TYPE_HEADER_CONTENTS "${CMAKE_MATCH_1}")
+
+string(REGEX REPLACE "\\{([^()]*)\\}" "
+{
+        #ifdef REALM_DEBUG
+        ${MATCHED_DEBUG_TYPE_HEADER_CONTENTS}
+        #else
+        ${MATCHED_RELEASE_TYPE_HEADER_CONTENTS}
+        #endif
+}
+    " MODIFIED_HEADER "${DEBUG_TYPE_HEADER_CONTENTS}")
+
+file(WRITE ${CURRENT_PACKAGES_DIR}/include/cpprealm/internal/bridge/bridge_types.hpp "${MODIFIED_HEADER}")
+
+file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
     "${CURRENT_PACKAGES_DIR}/debug/share"
 )
